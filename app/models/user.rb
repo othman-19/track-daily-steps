96753739668b785f37cd06adbe0ddda3dfb6fffb
othmane <othman-19@hotmail.fr>
2020-02-15 20:56:53 +0100

    add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+  before_save :downcase_email
+
+  validates :name, presence: true, length: { maximum: 50 }, allow_blank: false
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: false, allow_blank: false
+  
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
 end
