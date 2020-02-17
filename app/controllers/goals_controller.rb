54dae27df5076530d52480c21a1bb2e97df7ffc9
othmane <othman-19@hotmail.fr>
2020-02-17 10:42:10 +0100

    add goals controller with index and show

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,22 @@
+class GoalsController < ApplicationController
+  def index
+    @goals = goal.all
+  end
+
+  def show
+    @goal = Goal.find_by(id: params[:id])
+  end
+
+  def create
+  end 
+  
+  def update
+  end
+
+  def destroy
+  end
+  private
+    def goal_params
+      params.require(:goal).permit(:description, :start)
+    end
+end
