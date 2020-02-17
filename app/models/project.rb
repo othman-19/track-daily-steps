256796feac3204dc049d3a1df3a430b731d088f6
othmane <othman-19@hotmail.fr>
2020-02-17 11:25:46 +0100

    add projects controller, add project validations and associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ApplicationRecord
+  validates :name, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
+  has_many :goals
+  has_many :users, through: :goals
 end
