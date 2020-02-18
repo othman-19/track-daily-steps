d2249ae2194be162f99a2c225d4f9e18b894680f
othmane <othman-19@hotmail.fr>
2020-02-18 12:40:32 +0100

    add App Goal Project components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
