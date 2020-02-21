e4949aea34e7c0b2dd7a888187e46d977b5bbb0a
othmane <othman-19@hotmail.fr>
2020-02-21 19:42:45 +0100

    fix devise registration blank name bug

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,5 +7,8 @@ module API
         render json: {}, status: 401
       end
     end
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_sign_up_params, if: :devise_controller?
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+  end
 end
